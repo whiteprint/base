d56bc557114fe43cdd0000b8ceb92c8be9cc2a25
Sam Smith <sam@mintcanary.com>
2019-07-12 15:55:07 +0100

    add colours, minify

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-custom-properties': {preserve: false}
+    'postcss-custom-properties': {preserve: false},
+    'cssnano': {}
   }
 }
